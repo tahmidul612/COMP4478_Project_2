68528b74ff066a810411de24dfb098ac1d5605ff
Tahmidul Islam <tahmidul612@gmail.com>
2023-03-26 15:34:27 -0400

    Menu UI updates and settings menu (#5)
    * added button functions
    
    Clicking settings button now opens the settings menu
    
    Added a back button in the settings menu
    
    * expose volume parameter
    
    * upload ui sprites
    
    * upload kenney fonts
    
    * update sprite slice/border
    
    * replace maincamera with the prefab
    
    * internal change
    
    * update ui
    
    Added ui assets to the buttons, changed fonts and some other minor tweaks
    
    * code comments

--- Assets/Scripts/SettingsController.cs
+++ Assets/Scripts/SettingsController.cs
@@ -10,27 +10,32 @@ public class SettingsController : MonoBehaviour
     public AudioMixer mainMixer;
     private void Start()
     {
+        // Get all resolutions
         resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
 
+        resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
 
         int currentResolutionIndex = 0;
+        // Add all resolutions to the dropdown
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + "Hz";
             options.Add(option);
-
+            // Check if the game screen resolution matches to one of the
+            // available resolutions and set it as the current resolution value
+            // in the dropdown
             if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
             {
                 currentResolutionIndex = i;
             }
         }
-
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
     }
+
+    // Set volume
     public void SetVolume(float volume)
     {
         mainMixer.SetFloat("mainVolume", volume);

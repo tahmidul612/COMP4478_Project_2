206d7694856731147941b4f97a7d0f754fbd6f45
tahmidul612 <tahmidul612@gmail.com>
2023-04-01 04:41:02 -0400

    only display current resolution in webgl

--- Assets/Scripts/SettingsController.cs
+++ Assets/Scripts/SettingsController.cs
@@ -21,9 +21,9 @@ public class SettingsController : MonoBehaviour
         displays = new List<Display>();
         displays.AddRange(Display.displays);
         displayDropdown.ClearOptions();
-        for (int i=0; i<displays.Count; i++)
+        for (int i = 0; i < displays.Count; i++)
         {
-            displayDropdown.options.Add(new TMPro.TMP_Dropdown.OptionData("Display " + (i+1).ToString()));
+            displayDropdown.options.Add(new TMPro.TMP_Dropdown.OptionData("Display " + (i + 1).ToString()));
         }
         displayDropdown.onValueChanged.AddListener(delegate { SetDisplay(displayDropdown.value); });
         displayDropdown.RefreshShownValue();
@@ -34,21 +34,27 @@ public class SettingsController : MonoBehaviour
         // populate Resolution[] list with 3 resolutions (360p, 720p and 1080p)
         int currentRefreshRate = Screen.currentResolution.refreshRate;
         Resolution currentResolution = new Resolution
-            {
-                width = Screen.width,
-                height = Screen.height,
-                refreshRate = currentRefreshRate
-            };
-        resolutions = new List<Resolution>();
-        resolutions.AddRange(new Resolution[]
         {
+            width = Screen.width,
+            height = Screen.height,
+            refreshRate = currentRefreshRate
+        };
+        resolutions = new List<Resolution>();
+        if (Application.platform != RuntimePlatform.WebGLPlayer)
+        {
+            resolutions.AddRange(new Resolution[]
+            {
             new Resolution {width = 640, height = 360, refreshRate = currentRefreshRate},
             new Resolution {width = 1280, height = 720, refreshRate = currentRefreshRate},
             new Resolution {width = 1920, height = 1080, refreshRate = currentRefreshRate},
             new Resolution {width = 2560, height = 1440, refreshRate = currentRefreshRate},
             new Resolution {width = 3840, height = 2160, refreshRate = currentRefreshRate},
             currentResolution
-        });
+            });
+        }
+        else {
+            resolutions.Add(currentResolution);
+        }
         resolutions = resolutions.Distinct().ToList();
         resolutions = resolutions.OrderBy(res => res.width).ToList();
         // Add all resolutions to the dropdown

f1e35cafeae59e0b5cc8ac8a554d56fbc0d35dbc
thebluebark <114772264+thebluebark@users.noreply.github.com>
2023-04-01 13:54:53 -0400

    Foreground sprites

---
+++ Assets/Scripts/TurnInvisibleOnLoad.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnInvisibleOnLoad : MonoBehaviour
+{
+    Renderer objectRenderer;
+    private Material currentMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentMat = gameObject.GetComponent<Renderer>().material;
+        Color oldColor = currentMat.color;
+        Color newColor = new Color(oldColor.r, oldColor.g, oldColor.b, 0.0f);
+        currentMat.SetColor("_Color", newColor);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

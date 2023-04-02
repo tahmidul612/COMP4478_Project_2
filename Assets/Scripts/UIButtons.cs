fcfeba7d16eedb9f2d03d1ac3e396bc74ec3c23c
tahmidul612 <tahmidul612@gmail.com>
2023-04-02 00:33:36 -0400

    fix pause button

--- Assets/Scripts/UIButtons.cs
+++ Assets/Scripts/UIButtons.cs
@@ -5,18 +5,23 @@ using UnityEngine.UI;
 public class UIButtons : MonoBehaviour
 {
     [SerializeField] private GameObject cornerButtonsPanel;
+    public static bool isPaused = false;
     private void Start()
     {
         // Fullscreen button
         Button fullScreenButton = cornerButtonsPanel.transform.Find("Fullscreen").GetComponent<Button>();
         fullScreenIcon(fullScreenButton, Screen.fullScreen);
         fullScreenButton.onClick.AddListener(delegate { SetFullScreen(fullScreenButton, Screen.fullScreen); });
+        
         // Pause button
         Button pauseButton = cornerButtonsPanel.transform.Find("Pause").GetComponent<Button>();
-        if (SceneManager.GetActiveScene().name == "MainGame") {
-            // TODO: add pause functionality
+        if (SceneManager.GetActiveScene().name.Contains("Level"))
+        {
+            pauseButton.gameObject.SetActive(true);
+            pauseButton.onClick.AddListener(delegate { PauseGame(); });
         }
-        else {
+        else
+        {
             pauseButton.gameObject.SetActive(false);
         }
     }
@@ -31,5 +36,10 @@ public class UIButtons : MonoBehaviour
         button.transform.GetChild(0).GetComponent<Image>().enabled = !value;
         // smaller
         button.transform.GetChild(1).GetComponent<Image>().enabled = value;
+    }
+    private void PauseGame()
+    {
+        Time.timeScale = isPaused ? 0 : 1;
+        // GameObject.Find("PauseMenu").GetComponent<Canvas>().enabled = true;
     }
 }

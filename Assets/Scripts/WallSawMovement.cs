36108d78d7c88c63c46bf7695b8650b3b888b98d
thebluebark <114772264+thebluebark@users.noreply.github.com>
2023-04-02 19:18:46 -0400

    Hazards!

---
+++ Assets/Scripts/WallSawMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSawMovement : MonoBehaviour
+{
+    private float speed = 2f;
+    private bool turned = false;
+    private float yVal;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private LayerMask checkpointLayer;
+    [SerializeField] private Transform sawDetector;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (checkpointTouched() && turned == false)
+        {
+            speed = speed * -1;
+            turned = true;
+            StartCoroutine(waitForMove());
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, speed);
+    }
+
+    private bool checkpointTouched()
+    {
+        return Physics2D.OverlapCircle(sawDetector.position, 1.5f, checkpointLayer);
+    }
+
+    IEnumerator waitForMove()
+    {
+        yield return new WaitForSeconds(0.2f);
+        turned = false;
+    }
+
+}

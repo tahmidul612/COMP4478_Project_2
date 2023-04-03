978beb23f8948fadcf6aaf6c39c2bafb1ceabdb0
Tahmidul Islam <tahmidul612@gmail.com>
2023-04-02 22:12:43 -0400

    display settings fixes (#19)
    * display switching option hidden on a webgl build
    
    * moved fullscreen switching to a separate script
    
    * move mainmixer to resources folder
    
    Helps load the mixer with Resources
    
    * removed dependency on manual reference settings
    
    Settings references to the settings components from the Editor is no longer required. SettingsController finds the required buttons/dropdowns etc. internally.
    
    * corner buttons converted to prefab
    
    * reload resolution dropdown when fullscreen is toggled
    
    Fixes wrong current resolution showing in WebGL
    
    * various tweaks to fit with the other fixes

---
+++ Assets/Scripts/FullscreenController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FullscreenController : MonoBehaviour
+{
+    public static Button button;
+    void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+    public void ToggleFullScreen()
+    {
+        bool isFullscreen = Screen.fullScreen;
+        Screen.fullScreen = !isFullscreen;
+        fullScreenIcon(!isFullscreen);
+        if (GameObject.Find("SettingsMenu").activeSelf)
+        {
+            SettingsController.setupResolutions();
+        }
+    }
+    public static void fullScreenIcon(bool value)
+    {
+        // larger
+        button.transform.GetChild(0).GetComponent<Image>().enabled = !value;
+        // smaller
+        button.transform.GetChild(1).GetComponent<Image>().enabled = value;
+    }
+}
